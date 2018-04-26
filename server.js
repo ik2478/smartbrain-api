d82163911d9c5a1a3e20178d5038a219b7839c0b
ik2478 <ik247@yahoo.com>
2018-04-26 16:10:33 -0500

    changing module name:body-parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
